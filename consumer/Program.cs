d1b11b0bb1a85f0804181b48c3b4393fd292a685
Adam Ralph <adam@adamralph.com>
2020-04-27 15:19:22 +0200

    add consuming project to prove spike

---
+++ consumer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // violates CA2000
+            new FileStream("", default);
+
+            // violates CA2007
+            await Console.Out.WriteLineAsync("hi");
+        }
+    }
+}
