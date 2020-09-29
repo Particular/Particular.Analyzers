9c1b6f2be73ec097692665ae4e163237c1c1bc74
Adam Ralph <adam@adamralph.com>
2020-09-29 18:16:00 +0200

    align ruleset and suppressions usage with other repos

--- src/Particular.CodeRules.Tests/GlobalSuppressions.cs
+++ src/Particular.CodeRules.Tests/GlobalSuppressions.cs
@@ -1,8 +1,3 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+﻿using System.Diagnostics.CodeAnalysis;
 
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Tests")]
+[assembly: SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "test project")]
