f9d24567d571e680499f886e7a5e5b23bc0b7572
Andreas Öhlund <andreas.ohlund@particular.net>
2021-08-10 08:31:02 +0200

    Type name analyzer (#118)
    * First draft of the analyzer
    
    * Add tests
    
    * Support delegates
    
    * fix test class name
    
    * tweaks
    
    Co-authored-by: Adam Ralph <adam@adamralph.com>

---
+++ src/Particular.Analyzers.Tests/TypeNameAnalyzerTests.cs
@@ -0,0 +1,76 @@
+﻿namespace Particular.Analyzers.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Particular.Analyzers.Tests.Helpers;
+    using Xunit;
+    using Xunit.Abstractions;
+    using Data = System.Collections.Generic.IEnumerable<object[]>;
+
+    public class TypeNameAnalyzerTests : AnalyzerTestFixture<TypeNameAnalyzer>
+    {
+        static readonly string @type = "{0} [|{1}|] {{ }}";
+
+        static readonly string @delegate = "delegate void [|{0}|]();";
+
+        public TypeNameAnalyzerTests(ITestOutputHelper output) : base(output) { }
+
+        static readonly List<string> interfaceKeywords = new List<string>
+        {
+           "interface",
+        };
+
+        static readonly List<string> nonInterfaceKeywords = new List<string>
+        {
+            "class",
+            "enum",
+            "struct",
+            "record",
+        };
+
+        static readonly List<string> interfaceNames = new List<string>
+        {
+            "ISomething",
+            "II",
+        };
+
+        static readonly List<string> nonInterfaceNames = new List<string>
+        {
+            "Something",
+            "International",
+            "I",
+        };
+
+        public static Data SadTypesData =>
+            nonInterfaceKeywords.SelectMany(keyword => interfaceNames.Select(name => (keyword, name))).ToData();
+
+        public static Data HappyTypesData =>
+            interfaceKeywords.SelectMany(keyword => interfaceNames.Select(name => (keyword, name)))
+            .Concat(nonInterfaceKeywords.SelectMany(keyword => nonInterfaceNames.Select(name => (keyword, name)))).ToData();
+
+        public static Data SadDelegatesData => interfaceNames.ToData();
+
+        public static Data HappyDelegatesData => nonInterfaceNames.ToData();
+
+        [Theory]
+        [MemberData(nameof(SadTypesData))]
+        public Task SadTypes(string keyword, string name) => Assert(GetTypeCode(@type, keyword, name), DiagnosticIds.NonInterfaceTypePrefixedWithI);
+
+        [Theory]
+        [MemberData(nameof(HappyTypesData))]
+        public Task HappyTypes(string keyword, string name) => Assert(GetTypeCode(@type, keyword, name));
+
+        [Theory]
+        [MemberData(nameof(SadDelegatesData))]
+        public Task SadDelegates(string name) => Assert(GetDelegateCode(@delegate, name), DiagnosticIds.NonInterfaceTypePrefixedWithI);
+
+        [Theory]
+        [MemberData(nameof(HappyDelegatesData))]
+        public Task HappyDelegates(string name) => Assert(GetDelegateCode(@delegate, name));
+
+        static string GetTypeCode(string template, string keyword, string name) => string.Format(template, keyword, name);
+
+        static string GetDelegateCode(string template, string name) => string.Format(template, name);
+    }
+}
---
+++ src/Particular.Analyzers/TypeNameAnalyzer.cs
@@ -0,0 +1,78 @@
+﻿namespace Particular.Analyzers
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Particular.Analyzers.Extensions;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class TypeNameAnalyzer : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+            DiagnosticDescriptors.NonInterfaceTypePrefixedWithI);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(
+                Analyze,
+                SyntaxKind.ClassDeclaration,
+                SyntaxKind.RecordDeclaration,
+                SyntaxKind.StructDeclaration,
+                SyntaxKind.EnumDeclaration,
+                SyntaxKind.DelegateDeclaration);
+        }
+
+        static void Analyze(SyntaxNodeAnalysisContext context)
+        {
+            if (!(context.Node is MemberDeclarationSyntax type))
+            {
+                return;
+            }
+
+            if (!TryGetIdentifier(type, out var identifier))
+            {
+                return;
+            }
+
+            var name = identifier.Text;
+
+            if (name.Length < 2)
+            {
+                return;
+            }
+
+            if (name[0] != 'I')
+            {
+                return;
+            }
+
+            if (!char.IsUpper(name[1]))
+            {
+                return;
+            }
+
+            context.ReportDiagnostic(DiagnosticDescriptors.NonInterfaceTypePrefixedWithI, identifier, name);
+        }
+
+        static bool TryGetIdentifier(MemberDeclarationSyntax member, out SyntaxToken identifier)
+        {
+            identifier = default;
+
+            switch (member)
+            {
+                case BaseTypeDeclarationSyntax type:
+                    identifier = type.Identifier;
+                    return true;
+                case DelegateDeclarationSyntax @delegate:
+                    identifier = @delegate.Identifier;
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
