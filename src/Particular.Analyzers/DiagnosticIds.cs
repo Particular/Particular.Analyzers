e031fe029f452b994c85be5d0a03b3f7a96e81ba
David Boike <david.boike@gmail.com>
2024-02-23 09:03:20 -0600

    Fix duplicated diagnostic id by moving StructuredLoggingWithRepeatedToken to 26 since Core already has a PR with pragmas for PS0025

--- src/Particular.Analyzers/DiagnosticIds.cs
+++ src/Particular.Analyzers/DiagnosticIds.cs
@@ -26,7 +26,7 @@
         public const string ImplicitCastFromDateTimeToDateTimeOffset = "PS0022";
         public const string NowUsedInsteadOfUtcNow = "PS0023";
         public const string NonInterfaceTypePrefixedWithI = "PS0024";
-        public const string StructuredLoggingWithRepeatedToken = "PS0025";
         public const string DictionaryHasUnsupportedKeyType = "PS0025";
+        public const string StructuredLoggingWithRepeatedToken = "PS0026";
     }
 }
