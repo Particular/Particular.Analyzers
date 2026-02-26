b6afbf5d05b6d8dee3151dd89dfbe063e08cd339
David Boike <david.boike@gmail.com>
2026-02-26 16:34:24 -0600

    Use the new package (alpha)

--- src/Particular.AnalyzerTesting/AnalyzerTestFixtureState.cs
+++
@@ -1,8 +0,0 @@
-namespace Particular.AnalyzerTesting;
-
-using System;
-
-static class AnalyzerTestFixtureState
-{
-    internal static readonly bool VerboseLogging = Environment.GetEnvironmentVariable("CI") != "true" || Environment.GetEnvironmentVariable("VERBOSE_TEST_LOGGING")?.ToLower() == "true";
-}
